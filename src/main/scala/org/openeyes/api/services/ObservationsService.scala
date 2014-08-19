37c52af0b654118602314d37036b1539f03060a2
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-08-19 15:46:45 +0100

    The ObservationsService deals with CRUD actions for Observations.

---
+++ src/main/scala/org/openeyes/api/services/ObservationsService.scala
@@ -0,0 +1,21 @@
+package org.openeyes.api.services
+
+import org.bson.types.ObjectId
+import org.openeyes.api.models.{Weight, Observation}
+
+/**
+ * Created by dave on 19/08/2014.
+ */
+object ObservationsService {
+
+  def listAll = {
+    Observation.findAll().toList
+  }
+
+  def create(weight: Int) = {
+    val _id = new ObjectId
+    val wght = Weight(weight)
+    val observation = Observation(_id, wght)
+  }
+
+}
