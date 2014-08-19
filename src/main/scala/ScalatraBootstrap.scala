348bbc16a7767e582f92f71b5dd0be2e2dc687d1
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-08-19 15:12:07 +0100

    Cleanup.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,7 +1,7 @@
-import org.openeyes.api._
+import javax.servlet.ServletContext
+
 import org.openeyes.api.controllers.ObservationsController
 import org.scalatra._
-import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
