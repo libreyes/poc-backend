920f470e79070079402146b720bbc6fce4d569aa
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-08-19 16:56:49 +0100

    Cleanup.

--- src/main/scala/org/openeyes/api/stacks/ApiStack.scala
+++ src/main/scala/org/openeyes/api/stacks/ApiStack.scala
@@ -1,15 +1,11 @@
 package org.openeyes.api.stacks
 
-import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.ScalatraServlet
 import org.scalatra.i18n.I18nSupport
 import org.scalatra.json.JacksonJsonSupport
-import org.scalatra.swagger.{SwaggerSupport, JacksonSwaggerBase}
+import org.scalatra.swagger.SwaggerSupport
 
 /**
  * Created by dave on 19/08/2014.
  */
-trait ApiStack extends ScalatraServlet with JacksonJsonSupport with I18nSupport with SwaggerSupport {
-
-
-}
+trait ApiStack extends ScalatraServlet with JacksonJsonSupport with I18nSupport with SwaggerSupport
