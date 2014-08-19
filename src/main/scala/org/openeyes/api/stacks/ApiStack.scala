699548dc337eedd23e26bfdd029df30d8636f6f1
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-08-19 16:16:22 +0100

    The ApiStack now has SwaggerSupport.

--- src/main/scala/org/openeyes/api/stacks/ApiStack.scala
+++ src/main/scala/org/openeyes/api/stacks/ApiStack.scala
@@ -1,12 +1,15 @@
 package org.openeyes.api.stacks
 
+import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.ScalatraServlet
 import org.scalatra.i18n.I18nSupport
 import org.scalatra.json.JacksonJsonSupport
+import org.scalatra.swagger.{SwaggerSupport, JacksonSwaggerBase}
 
 /**
  * Created by dave on 19/08/2014.
  */
-trait ApiStack extends ScalatraServlet with JacksonJsonSupport with I18nSupport {
+trait ApiStack extends ScalatraServlet with JacksonJsonSupport with I18nSupport with SwaggerSupport {
+
 
 }
