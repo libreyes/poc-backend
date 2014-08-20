cd5f67a71a0fb5e61b2976b85bca510bce0ef920
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-08-20 13:14:00 +0100

    Adding the MongoDb ID handling module from json4s.

--- project/build.scala
+++ project/build.scala
@@ -25,6 +25,7 @@ object OpeneyesBuild extends Build {
         "com.novus" %% "salat" % "1.9.8",
         "jp.sf.amateras" %% "scalatra-forms" % "0.1.0",
         "org.json4s"   %% "json4s-jackson" % "3.2.9",
+        "org.json4s" %% "json4s-mongo" % "3.2.9",
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-json" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
