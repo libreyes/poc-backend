f1ba9a251cb68bfa2143abae1190572f8c40e73b
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-09-02 09:45:10 +0100

    Mounting the js provider servlet.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,5 +1,6 @@
 import javax.servlet.ServletContext
 
+import jp.sf.amateras.scalatra.forms.ValidationJavaScriptProvider
 import org.openeyes.api.controllers.{ResourcesApp, OpeneyesSwagger, LaserEventsController}
 import org.scalatra._
 
@@ -9,7 +10,8 @@ class ScalatraBootstrap extends LifeCycle {
 
 
   override def init(context: ServletContext) {
-    context.mount (new ResourcesApp, "/api-docs")
     context.mount(new LaserEventsController, "/laser-events", "laser-events")
+    context.mount (new ResourcesApp, "/api-docs")
+    context.mount(new ValidationJavaScriptProvider, "/assets/js/*")
   }
 }
