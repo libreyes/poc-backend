b16fea72d133eab61f7c05248cfa171f6c707c3a
jam13 <jamie.neil@gmail.com>
2014-09-03 17:16:30 +0100

    Add swagger markup to laser event

--- src/main/scala/org/openeyes/api/controllers/LaserEventsController.scala
+++ src/main/scala/org/openeyes/api/controllers/LaserEventsController.scala
@@ -3,14 +3,16 @@ package org.openeyes.api.controllers
 import org.json4s.mongo.ObjectIdSerializer
 import org.json4s.{DefaultFormats, Formats}
 import org.openeyes.api.forms.LaserEventForm
+import org.openeyes.api.models.LaserEvent
 import org.openeyes.api.services.LaserEventService
 import org.openeyes.api.stacks.ApiStack
-import org.scalatra.swagger.Swagger
+import org.scalatra.swagger.{DataType, ParamType, Parameter, Swagger}
+import org.scalatra.{NotFound, Ok}
 
 
 class LaserEventsController(implicit val swagger: Swagger) extends ApiStack {
 
-  protected val applicationDescription = "CRUD operations for LaserEvents"
+  protected val applicationDescription = "The LaserEvent API"
 
   protected implicit val jsonFormats: Formats = DefaultFormats + new ObjectIdSerializer
 
@@ -18,20 +20,54 @@ class LaserEventsController(implicit val swagger: Swagger) extends ApiStack {
     contentType = formats("json")
   }
 
-  get("/") {
+  val list = (apiOperation[List[LaserEvent]]("listLaserEvents")
+    notes "Lists all known LaserEvents"
+    summary "List LaserEvents"
+    )
+
+  get("/", operation(list)) {
     LaserEventService.findAll
   }
 
-  get("/:id") {
-    LaserEventService.find(params("id"))
+  val get = (apiOperation[LaserEvent]("getLaserEvent")
+    notes "Get a LaserEvent by ID"
+    parameters (
+    Parameter("id", DataType.String, Some("The ID of the LaserEvent to retrieve"), None, ParamType.Path, required = true)
+    )
+    summary "Get LaserEvent"
+    )
+
+  get("/:id", operation(get)) {
+    val id = params("id")
+    LaserEventService.find(id) match {
+      case Some(laserEvent) => Ok(laserEvent)
+      case None => NotFound("{ \"error\": \"No laser event found for id '" + id + "'.\" }")
+    }
   }
 
-  post("/") {
+  val post = (apiOperation[LaserEvent]("createLaserEvent")
+    notes "Create a LaserEvent"
+    parameters (
+      Parameter("body", DataType[LaserEventForm], Some("The LaserEvent content"), None, ParamType.Body, required = true)
+    )
+    summary "Create LaserEvent"
+    )
+
+  post("/", operation(post)) {
     val resource = parsedBody.extract[LaserEventForm]
     LaserEventService.create(resource)
   }
 
-  put("/:id") {
+  val put = (apiOperation[LaserEvent]("createLaserEvent")
+    notes "Update a LaserEvent"
+    parameters (
+      Parameter("id", DataType.String, Some("The ID of the LaserEvent to update"), None, ParamType.Path, required = true),
+      Parameter("body", DataType[LaserEventForm], Some("The LaserEvent content"), None, ParamType.Body, required = true)
+    )
+    summary "Update LaserEvent"
+    )
+
+  put("/:id", operation(put)) {
     val resource = parsedBody.extract[LaserEventForm]
     LaserEventService.update(params("id"), resource)
   }
