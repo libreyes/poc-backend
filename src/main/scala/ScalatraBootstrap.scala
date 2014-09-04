f94c1b84619c6ad87acdd4ff9c78d9c8da4eff93
jam13 <jamie.neil@gmail.com>
2014-09-04 11:24:28 +0100

    Fix api resource names

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,6 +1,5 @@
 import javax.servlet.ServletContext
 
-import jp.sf.amateras.scalatra.forms.ValidationJavaScriptProvider
 import org.openeyes.api.controllers._
 import org.scalatra._
 
@@ -10,11 +9,10 @@ class ScalatraBootstrap extends LifeCycle {
 
   override def init(context: ServletContext) {
     context.mount(new ResourcesApp, "/api-docs")
-    context.mount(new LaserEventsController, "/laser-event", "laser-event")
-    context.mount(new LaserOperatorsController, "/laser-operator", "laser-operator")
-    context.mount(new LasersController, "/laser", "laser")
-    context.mount(new PatientsController, "/patient", "patient")
-    context.mount(new SitesController, "/site", "site")
-    context.mount(new ValidationJavaScriptProvider, "/assets/js/*")
+    context.mount(new LaserEventsController, "/LaserEvent")
+    context.mount(new LaserOperatorsController, "/LaserOperator")
+    context.mount(new LasersController, "/Laser")
+    context.mount(new PatientsController, "/Patient")
+    context.mount(new SitesController, "/Site")
   }
 }
