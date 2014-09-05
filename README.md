3960f189c1fd51ca709080d92376e798575e1ffa
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-09-05 14:50:30 +0100

    Wrote an import script to get MongoDb set up, and documented it.

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ $ ./sbt
 ```
 
 If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
+
+
+## Dependencies ##
+
+* mongodb
+
+## Sample Data ##
+
+There are mongodb objects in the directory `docs/sample`. You can load them by running:
+
+```
+docs/sample/import.sh
+```
+
---
+++ docs/sample/import.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+mongoimport --db openeyes --collection lasers --file lasers.json
+mongoimport --db openeyes --collection patients --file patients.json
+mongoimport --db openeyes --collection procedures --file procedures.json
+mongoimport --db openeyes --collection sites --file sites.json
+mongoimport --db openeyes --collection siteslasers --file siteslasers.json
+
