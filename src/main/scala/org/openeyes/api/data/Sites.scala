4962c687838236574831dc6631a6b19fd26be676
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-09 12:12:32 +0100

    A Sites object to retrieve sites.

--- src/main/scala/org/openeyes/api/data/Sites.scala
+++ src/main/scala/org/openeyes/api/data/Sites.scala
@@ -1,8 +1,23 @@
 package org.openeyes.api.data
 
-/**
- * Created by stu on 09/09/2014.
- */
-object Sites {
+import org.openeyes.api.models.Site
+
+import scala.slick.driver.MySQLDriver.simple._
+
+
+
+object Sites extends Table[Site]("site") {
+  def id = column[Int]("id", O.PrimaryKey, O.AutoInc) // This is the primary key column
+  def codeValue = column[String]("short_name")
+  def label = column[String]("name")
+  def systemId = column[String]("remote_id")
+
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = id.? ~ codeValue ~ label ~ systemId <> (Site, Site.unapply _)
+
+  val findById = for {
+    id <- Parameters[Int]
+    c <- this if c.id is id
+  } yield c
 
 }
