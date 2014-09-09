1a9834a29eed3801b792dd807cb2d51648176c85
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-09 11:01:41 +0100

    Switched back to MySQL

--- project/build.scala
+++ project/build.scala
@@ -41,7 +41,7 @@ object OpeneyesBuild extends Build {
         "org.scalatra" %% "scalatra-swagger"  % ScalatraVersion,
         "com.typesafe.slick" %% "slick" % "1.0.1",
         "org.slf4j" % "slf4j-nop" % "1.6.4",
-        "com.h2database" % "h2" % "1.3.166",
+        "mysql" % "mysql-connector-java" % "5.1.32",
         "c3p0" % "c3p0" % "0.9.1.2",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.1.3.v20140225" % "container",
