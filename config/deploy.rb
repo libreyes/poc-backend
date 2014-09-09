7370a5ecce9c965c89ce68666b4e61d4f965917e
Alex Kinnane <alex.kinnane@headlondon.com>
2014-09-09 15:36:47 +0100

    fixing deploy script, correcting indent levels, moving interactive tag deploy to seperate deploy:tag task

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_via, :remote_cache
 namespace :deploy do
   desc <<-DESC
   Does a full deploy using the tasks specified.
-  DESC
+    DESC
   task :full do
     transaction do
       remove_cached_copy
@@ -21,15 +21,20 @@ namespace :deploy do
       upload_war
       create_symlink
     end
-      # We are not able to roll back while using the Tomcat manager as
-      # capistrano only knows about file operations.
-      undeploy_app
-      deploy_app
+    # We are not able to roll back while using the Tomcat manager as
+    # capistrano only knows about file operations.
+    undeploy_app
+    deploy_app
+  end
+
+  task :tag do
+    set_tag
+    full
   end
 
   desc <<-DESC
   Removes the cached-copy folder.
-  DESC
+    DESC
   task :remove_cached_copy do
     run("rm -rf #{deploy_to}/shared/cached-copy")
     # run("mkdir #{deploy_to}/shared/cached-copy")
@@ -49,7 +54,7 @@ namespace :deploy do
 
   desc <<-DESC
   Upload war to server.
-  DESC
+    DESC
   task :upload_war do
     top.upload("target/scala-2.11/openeyes_2.11-0.1.4.war",
                "#{release_path}/openeyes_2.11-0.1.4.war")
@@ -57,17 +62,30 @@ namespace :deploy do
 
   desc <<-DESC
   Upload war to server.
-  DESC
+    DESC
   task :undeploy_app do
     puts "==================Undeploy war======================"#Line 24
-    run "curl --user #{tomcat_manager}:#{tomcat_manager_password} http://localhost:8080/manager/text/undeploy?path=/ROOT"
+    run "curl --user #{tomcat_manager}:#{tomcat_manager_password} http://localhost:8080/manager/text/undeploy?path=/"
   end
 
   desc <<-DESC
   Upload war to server.
-  DESC
+    DESC
   task :deploy_app do
     puts "==================Deploy war to Tomcat======================" #Line 26
     run "curl --upload-file #{current_path}/openeyes_2.11-0.1.4.war --user #{tomcat_manager}:#{tomcat_manager_password} http://localhost:8080/manager/text/deploy?path=/ROOT"
+  end
+
+  desc <<-DESC
+  Set the tag for a deploy.
+    DESC
+  task :set_tag do
+    set :branch do
+      default_tag = `git tag`.split("\n").last
+
+      tag = Capistrano::CLI.ui.ask "Tag to deploy (make sure to push the tag first): [#{default_tag}] "
+      tag = default_tag if tag.empty?
+      tag
+    end
   end
 end
--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -5,13 +5,9 @@ role :db, "178.79.188.31", :primary => true
 set :environment, :uat
 
 set :scm, "git"
-set :branch do
-  default_tag = `git tag`.split("\n").last
+branch = `git tag`.split("\n").last
+set :branch, branch
 
-  tag = Capistrano::CLI.ui.ask "Tag to deploy (make sure to push the tag first): [#{default_tag}] "
-  tag = default_tag if tag.empty?
-  tag
-end
 set :repository, 'https://openeyes:B3gjKuRZbeoCNhRTB4yM@gitbucket.headlondon.com/git/openeyes/openeyes-experimental-api.git'
 set :deploy_to, "/home/openeyes/public_html/#{environment}/api/"
 set :user, "openeyes" # the ssh user we'll deploy as.
