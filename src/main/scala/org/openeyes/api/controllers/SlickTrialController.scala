bc12455a478dff5d8627cde589b039a3b4cea2d1
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-09 11:02:25 +0100

    A working Suppliers API controller using the case class.

--- src/main/scala/org/openeyes/api/controllers/SlickTrialController.scala
+++ src/main/scala/org/openeyes/api/controllers/SlickTrialController.scala
@@ -1,14 +1,27 @@
 package org.openeyes.api.controllers
 
-import scala.slick.driver.H2Driver.simple._
+import org.json4s.mongo.ObjectIdSerializer
+import org.json4s.{DefaultFormats, Formats}
+import org.openeyes.api.stacks.ApiStack
+import org.scalatra.swagger.Swagger
+
+import scala.slick.driver.MySQLDriver.simple._
 import Database.threadLocalSession
-import org.openeyes.api.data.{Suppliers, Coffees}
+import org.openeyes.api.data.{Supplier, Suppliers, Coffees}
 import org.scalatra.ScalatraServlet
 
 /**
  * Created by stu on 09/09/2014.
  */
-class SlickTrialController(db: Database) extends ScalatraServlet {
+class SlickTrialController(db: Database)(implicit val swagger: Swagger) extends ApiStack {
+
+  protected val applicationDescription = "The Site API."
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
 
   get("/create-tables") {
     db withSession {
@@ -19,9 +32,14 @@ class SlickTrialController(db: Database) extends ScalatraServlet {
   get("/load-data") {
     db withSession {
       // Insert some suppliers
-      Suppliers.insert(101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199")
-      Suppliers.insert(49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460")
-      Suppliers.insert(150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966")
+      val sup1 = Supplier(None, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199")
+      val sup2 = Supplier(None, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460")
+      val sup3 = Supplier(None,  "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966")
+
+      Suppliers.insert(sup1)
+      Suppliers.insert(sup2)
+      Suppliers.insert(sup3)
+
 
       // Insert some coffees (using JDBC's batch insert feature, if supported by the DB)
       Coffees.insertAll(
@@ -49,6 +67,13 @@ class SlickTrialController(db: Database) extends ScalatraServlet {
 
       contentType = "text/html"
       q3.list.map { case (s1, s2) => "  " + s1 + " supplied by " + s2 } mkString "<br />"
+    }
+  }
+
+  get("/suppliers") {
+    db withSession {
+      val q = Query(Suppliers)
+      q.list
     }
   }
 
