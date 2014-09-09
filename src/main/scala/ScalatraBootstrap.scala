b960439e4fd0e33ab10768e43d5450bbca31a86f
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-09 10:27:40 +0100

    Adding DB init code to ScalatraBootstrap

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,13 +1,19 @@
 import javax.servlet.ServletContext
 
+import com.mchange.v2.c3p0.ComboPooledDataSource
 import org.openeyes.api.controllers._
 import org.scalatra._
+import scala.slick.session.Database
 
 class ScalatraBootstrap extends LifeCycle {
 
   implicit val swagger = new OpeneyesSwagger
 
+  val cpds = new ComboPooledDataSource
+
   override def init(context: ServletContext) {
+    val db = Database.forDataSource(cpds)
+
     context.mount(new ResourcesApp, "/api-docs")
     context.mount(new LaserEventController, "/LaserEvent", "LaserEvent")
     context.mount(new LaserOperatorController, "/LaserOperator", "LaserOperator")
@@ -15,5 +21,15 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new PatientController, "/Patient", "Patient")
     context.mount(new ProcedureController, "/Procedure", "Procedure")
     context.mount(new SiteController, "/Site", "Site")
+    context.mount(new SlickTrialController(db), "/db/*")
+  }
+
+  private def closeDbConnection() {
+    cpds.close
+  }
+
+  override def destroy(context: ServletContext) {
+    super.destroy(context)
+    closeDbConnection
   }
 }
