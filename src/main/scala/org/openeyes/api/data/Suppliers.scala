a512081d9a08df1462a99abba21e8024a68af406
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-09 11:01:59 +0100

    Set up a Suppliers case class

--- src/main/scala/org/openeyes/api/data/Suppliers.scala
+++ src/main/scala/org/openeyes/api/data/Suppliers.scala
@@ -1,15 +1,16 @@
 package org.openeyes.api.data
 
-import scala.slick.driver.H2Driver.simple._
+import scala.slick.driver.MySQLDriver.simple._
 import Database.threadLocalSession
 
+case class Supplier(id: Option[Int], name: String, street: String, city: String, state: String, zip: String)
 
 /**
  * Created by stu on 09/09/2014.
  */
 // Definition of the SUPPLIERS table
-object Suppliers extends Table[(Int, String, String, String, String, String)]("SUPPLIERS") {
-  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+object Suppliers extends Table[Supplier]("SUPPLIERS") {
+  def id = column[Int]("SUP_ID", O.PrimaryKey, O.AutoInc) // This is the primary key column
   def name = column[String]("SUP_NAME")
   def street = column[String]("STREET")
   def city = column[String]("CITY")
@@ -17,5 +18,6 @@ object Suppliers extends Table[(Int, String, String, String, String, String)]("S
   def zip = column[String]("ZIP")
 
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = id ~ name ~ street ~ city ~ state ~ zip
+  def * = id.? ~ name ~ street ~ city ~ state ~ zip <> (Supplier, Supplier.unapply _)
 }
+
