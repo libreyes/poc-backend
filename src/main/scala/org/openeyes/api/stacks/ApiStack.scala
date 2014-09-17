0a8e75e009cf5b8d601b1ba39168c8fb6756eb1f
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-09-17 14:13:51 +0100

    Adding LoggerStack to ApiStack extensions.

--- src/main/scala/org/openeyes/api/stacks/ApiStack.scala
+++ src/main/scala/org/openeyes/api/stacks/ApiStack.scala
@@ -1,5 +1,6 @@
 package org.openeyes.api.stacks
 
+import org.openeyes.api.stacks.com.constructiveproof.remotable.stacks.LoggerStack
 import org.scalatra.ScalatraServlet
 import org.scalatra.i18n.I18nSupport
 import org.scalatra.json.JacksonJsonSupport
@@ -8,4 +9,4 @@ import org.scalatra.swagger.SwaggerSupport
 /**
  * Created by dave on 19/08/2014.
  */
-trait ApiStack extends ScalatraServlet with JacksonJsonSupport with I18nSupport with SwaggerSupport
+trait ApiStack extends ScalatraServlet with JacksonJsonSupport with I18nSupport with SwaggerSupport with LoggerStack
