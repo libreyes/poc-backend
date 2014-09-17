a5cc432adb3e7c7d550a782f003f506be5162280
jam13 <jamie.neil@gmail.com>
2014-09-17 15:37:10 +0100

    Added workflows sample data

--- docs/sample/import.sh
+++ docs/sample/import.sh
@@ -4,4 +4,5 @@ mongoimport --db openeyes --collection patients --file patients.json
 mongoimport --db openeyes --collection procedures --file procedures.json
 mongoimport --db openeyes --collection sites --file sites.json
 mongoimport --db openeyes --collection siteslasers --file siteslasers.json
+mongoimport --db openeyes --collection workflows --file workflows.json
 
