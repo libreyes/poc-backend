57a004d1a3af674bf269e7d04ffdf4f4dbffd671
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-24 17:46:11 +0100

    Moved Models that are actually now moved into their own files.

--- src/main/scala/org/openeyes/api/models/Models.scala
+++ src/main/scala/org/openeyes/api/models/Models.scala
@@ -7,14 +7,9 @@ import com.novus.salat.dao.{ModelCompanion, SalatDAO}
 import com.novus.salat.global._
 import org.bson.types.ObjectId
 
-/**
- * Created by dave on 19/08/14.
- */
 
-case class ApiError(message: String)
 
-case class Encounter(@Key("_id") _id: ObjectId, patientId: ObjectId, createdAt: Long, elements: List[Element],
-                     ticketId: Option[ObjectId], stepIndex: Option[Int])
+
 
 case class Episode(events: Option[List[LaserEvent]])
 
@@ -34,18 +29,6 @@ case class Procedure(id: String, codeValue: String, label: String, systemId: Str
 case class Site(id: String, codeValue: String, label: String, systemId: String)
 
 case class TreatedEye(procedures: List[Procedure], anteriorSegment: AnteriorSegment)
-
-object Encounter extends ModelCompanion[Encounter, ObjectId] {
-
-  val collection = MongoConnection()("openeyes")("encounters")
-  val dao = new SalatDAO[Encounter, ObjectId](collection = collection) {}
-
-  def findAllForPatient(patientId: String): Seq[Encounter] = {
-    find(
-      MongoDBObject("patientId" -> new ObjectId(patientId))
-    ).toSeq
-  }
-}
 
 object LaserEvent extends ModelCompanion[LaserEvent, ObjectId] {
 
