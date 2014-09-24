c6647627084a0a5dfd4dade6a26358c6174d4454
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-24 16:26:58 +0100

    Updated import.sh script to clear the DB before importing.

--- docs/sample/import.sh
+++ docs/sample/import.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 # NOTE: encounters.json can't be imported via the importer
+mongo openeyes --eval "db.dropDatabase();"
 mongoimport --db openeyes --collection patients --file patients.json
 mongoimport --db openeyes --collection tickets --file tickets.json
 mongoimport --db openeyes --collection workflows --file workflows.json
