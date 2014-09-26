6bb56649eff4cfe93bc217b8fa71b3bd62ca3596
Stuart Chinery <stuart.chinery@headlondon.com>
2014-09-26 10:19:02 +0100

    Added some minor (and basic) formatting of the ApiError message for when a MappingException is thrown

--- src/main/scala/org/openeyes/api/models/ApiError.scala
+++ src/main/scala/org/openeyes/api/models/ApiError.scala
@@ -4,4 +4,7 @@ package org.openeyes.api.models
  * Created by dave on 19/08/14.
  */
 
-case class ApiError(message: String)
+case class ApiError(var message: String) {
+  // TODO: Probably a nicer way to do this
+  message = message.replace("\n", ". ")
+}
