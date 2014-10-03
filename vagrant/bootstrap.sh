94846e115c37a0e7bb5a41fb78dec3c6569aa81c
Richard Willis <willis.rh@gmail.com>
2014-10-03 11:26:13 +0100

    Vagrant update

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,16 +6,11 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
 
-# Install Java & Mongo
+# Install dependencies
 apt-get install -y \
-  openjdk-7-jdk \
-  mongodb-org-server=2.6.1 mongodb-org-shell=2.6.1 mongodb-org-tools=2.6.1
+	openjdk-7-jdk \
+	mongodb-org-server=2.6.1 mongodb-org-shell=2.6.1 mongodb-org-tools=2.6.1\
+	imagemagick
 
-# Run as user vagrant from here
-su - vagrant
-
-# Import sample data
-cd /app/docs/sample && ./import.sh
-
-# Start the api service
-# cd /app && ./sbt ~container:start &
+su -c "cd /app/docs/sample && ./import.sh" vagrant
+su -c "cd /app && ./sbt update" vagrant
