100fc43321881ec28b0d32401057f743993f12c6
Stuart Chinery <stuart.chinery@headlondon.com>
2014-10-08 14:18:49 +0100

    Switched from FlatSpec to WordSpec.

--- src/test/scala/org/openeyes/api/services/EncounterServiceSpec.scala
+++ src/test/scala/org/openeyes/api/services/EncounterServiceSpec.scala
@@ -2,11 +2,11 @@ package org.openeyes.api.services
 
 import org.bson.types.ObjectId
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.FlatSpec
+import org.scalatest.WordSpec
 import org.openeyes.api.forms.EncounterForm
 import org.openeyes.api.models.EncounterDao
 
-class EncounterServiceSpec extends FlatSpec with MockFactory {
+class EncounterServiceSpec extends WordSpec with MockFactory {
   private class Fixture {
     val mockTicketService = mock[workflow.TicketService]
     val mockEncounterDao = stub[EncounterDao]
@@ -17,12 +17,16 @@ class EncounterServiceSpec extends FlatSpec with MockFactory {
     }
   }
 
-  "The Encounter Service" should "update the ticket step when creating an encounter associated with a ticket" in new Fixture {
-    val patientId = new ObjectId
-    val ticketId = new ObjectId
+  "The Encounter Service" should {
+    "update the ticket step when creating an encounter associated with a ticket" in {
+      new Fixture {
+        val patientId = new ObjectId
+        val ticketId = new ObjectId
 
-    (mockTicketService.updateStepIndexOrComplete _).expects(ticketId, 42)
+        (mockTicketService.updateStepIndexOrComplete _).expects(ticketId, 42)
 
-    TestEncounterService.create(EncounterForm(patientId.toString, Nil, Some(ticketId.toString), Some(42)))
+        TestEncounterService.create(EncounterForm(patientId.toString, Nil, Some(ticketId.toString), Some(42)))
+      }
+    }
   }
 }
