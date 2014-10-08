9d75992327a7b1eca7aca57b5ea79e1d0c7641d5
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-10-08 11:12:29 +0100

    Fix test.
     * Use a fixture class to work around https://github.com/paulbutcher/ScalaMock/issues/25
     * Use a stub rather than a mock for the DAO so unexpected calls are allowed
     * Fix exception caused by invalid object ID - this was not reported by ScalaMock

--- src/test/scala/org/openeyes/api/services/EncounterServiceSpec.scala
+++ src/test/scala/org/openeyes/api/services/EncounterServiceSpec.scala
@@ -7,19 +7,22 @@ import org.openeyes.api.forms.EncounterForm
 import org.openeyes.api.models.EncounterDao
 
 class EncounterServiceSpec extends FlatSpec with MockFactory {
-  private val mockTicketService = mock[workflow.TicketService]
-  private val mockEncounterDao = mock[EncounterDao]
+  private class Fixture {
+    val mockTicketService = mock[workflow.TicketService]
+    val mockEncounterDao = stub[EncounterDao]
 
-  object TestEncounterService extends EncounterService {
-    protected val ticketService = mockTicketService
-    protected val encounterDao = mockEncounterDao
+    object TestEncounterService extends EncounterService {
+      protected val ticketService = mockTicketService
+      protected val encounterDao = mockEncounterDao
+    }
   }
 
-  "The Encounter Service" should "update the ticket step when creating an encounter associated with a ticket" in {
+  "The Encounter Service" should "update the ticket step when creating an encounter associated with a ticket" in new Fixture {
+    val patientId = new ObjectId
     val ticketId = new ObjectId
 
     (mockTicketService.updateStepIndexOrComplete _).expects(ticketId, 42)
 
-    TestEncounterService.create(EncounterForm("myPatientId", Nil, Some(ticketId.toString), Some(42)))
+    TestEncounterService.create(EncounterForm(patientId.toString, Nil, Some(ticketId.toString), Some(42)))
   }
 }
