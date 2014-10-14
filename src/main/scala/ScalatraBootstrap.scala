eb0075e1caa74d4027a0b35d164da919bd1a100b
Stuart Chinery <stuart.chinery@headlondon.com>
2014-10-14 15:17:41 +0100

    Commented out OctScan code in the ScalatraBootstrap.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -10,7 +10,8 @@ import org.scalatra._
 class ScalatraBootstrap extends LifeCycle {
 
   implicit val swagger = new OpenEyesSwagger
-  val pool: ExecutorService = Executors.newFixedThreadPool(5)
+
+//  val pool: ExecutorService = Executors.newFixedThreadPool(5)
 
   override def init(context: ServletContext) {
     context.mount(new ApiDocsController, "/api-docs")
@@ -20,28 +21,27 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new TicketController, "/Ticket", "Ticket")
     context.mount(new WorkflowController, "/Workflow", "Workflow")
 
-    // TODO: Need to fix this so we can use the auto compile.
-    try {
-      pool.execute(new Handler())
-    } catch {
-      case e: Exception =>
-        pool.shutdown()
-        pool.execute(new Handler())
-    }
+//    try {
+//      pool.execute(new Handler())
+//    } catch {
+//      case e: Exception =>
+//        pool.shutdown()
+//        pool.execute(new Handler())
+//    }
   }
 
-  override def destroy(context: ServletContext) {
-    pool.shutdown()
-  }
-
-  class Handler() extends Runnable {
-    def run() {
-      val args: Array[String] = Array("-b", "OPENEYES:11112", "--directory", "tmp/DICOMFiles/")
-      OctScan.main(args, (file: File) => {
-        OctScan.process(file)
-      })
-    }
-  }
+//  override def destroy(context: ServletContext) {
+//    pool.shutdown()
+//  }
+//
+//  class Handler() extends Runnable {
+//    def run() {
+//      val args: Array[String] = Array("-b", "OPENEYES:11112", "--directory", "tmp/DICOMFiles/")
+//      OctScan.main(args, (file: File) => {
+//        OctScan.process(file)
+//      })
+//    }
+//  }
 }
 
 
